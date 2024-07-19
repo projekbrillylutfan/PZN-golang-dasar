502a9f1d2481eae3e8020f75219d3aabdb2c66fe
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-07-19 14:32:37 +0700

    susah sekali golang ini

--- belajar-golang-goroutines/channel_test.go
+++ belajar-golang-goroutines/channel_test.go
@@ -22,3 +22,57 @@ func TestCreateChannel(t *testing.T) {
 
 	defer close(channel)
 }
+
+func GiveMeResponse (channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "ayam goreng"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveMeResponse(channel)
+
+	data := <- channel
+	fmt.Println(data)
+	close(channel)
+}
+
+func OnlyIn (channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "ayam goreng"
+}
+
+func OnlyOut (channel <-chan string) {
+	data:= <- channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	go OnlyIn(channel)
+
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+	close(channel)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func ()  {
+		channel <- "Hello"
+		channel <- "World"
+		channel <- "Go"
+	} ()
+
+	go func ()  {
+		fmt.Println(<- channel)
+		fmt.Println(<- channel)
+		fmt.Println(<- channel)
+	} ()
+	fmt.Println(cap(channel))
+	fmt.Println(len(channel))
+}
